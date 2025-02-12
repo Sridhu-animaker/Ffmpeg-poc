01519f27b93be2853204a48c906ceca0fcffb7cc
ANIMCHNLAP292 <animchnlap292@ANIMCHNLAP292s-MacBook-Pro.local>
2025-02-12 10:44:23 +0530

    Zoom pan feature implementation.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ debugger
     const outputFile = `output/${ptX}x${ptY}.mp4`
     
     // Construct the FFmpeg command. Newlines have been escaped with backslashes.
-    const ffmpegCommand = `ffmpeg -i input.mp4 -filter_complex "[0:v]split=4[lefttop][leftbottom][righttop][rightbottom];[lefttop]crop=${X}:${Y}:0:0,zoompan=z='if(between(time,0,3),1+((4-1)/3)*(time-0),if(between(time,5,8),4-((4-1)/3)*(time-5),if(between(time,3,5),4,1)))':x=${X}:y=${Y}:d=1:s=${X}x${Y}:fps=25[lt];[leftbottom]crop=${X}:${ih - Y}:0:${Y},zoompan=z='if(between(time,0,3),1+((4-1)/3)*(time-0),if(between(time,5,8),4-((4-1)/3)*(time-5),if(between(time,3,5),4,1)))':x=${X}:y=0:d=1:s=${X}x${ih - Y}:fps=25[lb];[righttop]crop=${iw - X}:${Y}:${X}:0,zoompan=z='if(between(time,0,3),1+((4-1)/3)*(time-0),if(between(time,5,8),4-((4-1)/3)*(time-5),if(between(time,3,5),4,1)))':x=0:y=${Y}:d=1:s=${iw - X}x${Y}:fps=25[rt];[rightbottom]crop=${iw - X}:${ih - Y}:${X}:${Y},zoompan=z='if(between(time,0,3),1+((4-1)/3)*(time-0),if(between(time,5,8),4-((4-1)/3)*(time-5),if(between(time,3,5),4,1)))':x=0:y=0:d=1:s=${iw - X}x${ih - Y}:fps=25[rb];color=c=ffffff:s=${iw}x${ih}:r=25:d=10,settb=1/10[bg];[bg][lt]overlay=0:0:enable='between(t,0,10)'[bglt];[bglt][lb]overlay=0:${Y}:enable='between(t,0,10)'[bglb];[bglb][rt]overlay=${X}:0:enable='between(t,0,10)'[bgrt];[bgrt][rb]overlay=${X}:${Y}:enable='between(t,0,10)'" -c:v libx264 -preset ultrafast ${outputFile}`;
+    const ffmpegCommand = `ffmpeg -i input.mp4 -filter_complex "[0:v]split=4[lefttop][leftbottom][righttop][rightbottom];[lefttop]crop=${X}:${Y}:0:0,zoompan=z='if(between(time,0,3),1+${(z-1)/3}*(time-0),if(between(time,5,8),${z}-${(z-1)/3}*(time-5),if(between(time,3,5),${z},1)))':x=${X}:y=${Y}:d=1:s=${X}x${Y}:fps=25[lt];[leftbottom]crop=${X}:${ih - Y}:0:${Y},zoompan=z='if(between(time,0,3),1+${(z-1)/3}*(time-0),if(between(time,5,8),${z}-${(z-1)/3}*(time-5),if(between(time,3,5),${z},1)))':x=${X}:y=0:d=1:s=${X}x${ih - Y}:fps=25[lb];[righttop]crop=${iw - X}:${Y}:${X}:0,zoompan=z='if(between(time,0,3),1+${(z-1)/3}*(time-0),if(between(time,5,8),${z}-${(z-1)/3}*(time-5),if(between(time,3,5),${z},1)))':x=0:y=${Y}:d=1:s=${iw - X}x${Y}:fps=25[rt];[rightbottom]crop=${iw - X}:${ih - Y}:${X}:${Y},zoompan=z='if(between(time,0,3),1+${(z-1)/3}*(time-0),if(between(time,5,8),${z}-${(z-1)/3}*(time-5),if(between(time,3,5),${z},1)))':x=0:y=0:d=1:s=${iw - X}x${ih - Y}:fps=25[rb];color=c=ffffff:s=${iw}x${ih}:r=25:d=10,settb=1/10[bg];[bg][lt]overlay=0:0:enable='between(t,0,10)'[bglt];[bglt][lb]overlay=0:${Y}:enable='between(t,0,10)'[bglb];[bglb][rt]overlay=${X}:0:enable='between(t,0,10)'[bgrt];[bgrt][rb]overlay=${X}:${Y}:enable='between(t,0,10)'" -c:v libx264 -preset ultrafast ${outputFile}`;
 
     console.log(ffmpegCommand);
 
@@ -21,17 +21,11 @@ debugger
 }
 
 
-// (C4, R3) = (1440, 405)
-// (C4, R3) = (1440, 675)
-// (C3, R2) = (960, 405)
-// (C2, R3) = (480, 675)
-
-
 // Assign the Values here.
 const inputWidth = 1920;
 const inputHeight = 1080;
 const Scale = 4;
-const ptX = 960;
-const ptY = 405;
+const ptX = 1440;
+const ptY = 675;
 
 zoomVideo(inputWidth, inputHeight, Scale, ptX, ptY);
